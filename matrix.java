3bc8faad56468e616923ee78aa0ffa5a90e501b1
turno47 <turnaallow12@gmail.com>
2016-10-30 14:55:40 +0600

    Add files via upload

---
+++ matrix.java
@@ -0,0 +1,92 @@
+
+
+public class matrix
+{
+    private static final int num_of_threads = 9;
+
+    public static void main (String args[])
+    {
+        int A[][] = {{1, 2, 4}, {2, 5, 10}, {1, 5, 7}};
+        int B[][] = {{10, 8, 1}, {2, 6, 7}, {3, 1, 5}};
+        int C[][] = new int [3][3];
+        int i, j;
+        int ThreadCount = 0;
+
+        Thread thrd[] = new Thread [num_of_threads];
+
+        try
+        {
+            for (i=0; i<3; i++)
+        {
+            for (j=0; j<3; j++)
+            {
+                thrd [ThreadCount] = new Thread (new Worker (i, j, A, B, C));
+
+                thrd [ThreadCount].start();
+                thrd [ThreadCount].join();
+
+                ThreadCount++;
+            }
+        }
+    }catch (Exception e)
+    {
+        System.out.println (e);
+    }
+
+    System.out.println ("A matrix : ");
+
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
+            System.out.print (A[i][j]+ " ");
+        }
+        System.out.println ();
+    }
+
+    System.out.println ("B matrix : ");
+
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
+            System.out.print (B[i][j]+ " ");
+        }
+        System.out.println ();
+    }
+
+    System.out.println ("C matrix : ");
+
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
+            System.out.print (C[i][j]+ " ");
+        }
+        System.out.println ();
+    }
+}
+}
+
+class Worker implements Runnable
+    {
+        private int i, j;
+        private int A[][], B[][], C[][];
+
+        public Worker (int i, int j, int A[][], int B[][], int C[][])
+        {
+            this.i = i;
+            this.j = j;
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public void run ()
+        {
+            for (int k=0; k<B.length; k++)
+            {
+                C[i][j] = C[i][j] + A[i][k] * B[k][j];
+            }
+        }
+    }
